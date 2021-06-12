4d7ca0d4d789d7d0c9d2589c3bcbb30f2f3b228a
stiven lovera <50520255+stivenlovera@users.noreply.github.com>
2021-06-11 20:32:30 -0400

    creacion de modelo tela

---
+++ model/Tela.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace inventario_telas.model
+{
+    public class Tela
+    {
+        public int id { get; set; }
+        public string marca { get; set; }
+        public int largo { get; set; }
+        public int tipo { get; set; }
+        public string color { get; set; }
+        public string procedencia { get; set; }
+        public bool estado { get; set; }
+    }
+}
