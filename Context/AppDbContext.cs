50840f8f086b57515e030a135a11c7a698f21b1a
stiven lovera <50520255+stivenlovera@users.noreply.github.com>
2021-06-11 21:16:58 -0400

    creacion de context para la comunicacion entre modelos

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace inventario_telas.Context
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option)
+        {
+
+        }
+    }
+}
---
+++ Controllers/TelaController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace inventario_telas.Controllers
+{
+    public class TelaController
+    {
+    }
+}
